833e8bd995cb13be645bc025130d9627dec8e499
Joseph Broome <josephlloydbroome@gmail.com>
2022-06-02 12:20:16 -0700

    updates

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -28,7 +28,28 @@ internal class Program
         int num2 = int.Parse(Console.ReadLine());
 
         int sum = Sum(num1, num2);
-        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($" the sum is {sum}");
+
+        int sum1 = Sum1(2,4);
+        Console.WriteLine($"The sum is {sum1}");
+        int sum2 = Sum2(2, 4, 6);
+        Console.WriteLine($"The sume is {sum2}");
+        int sum3 = Sum3(1, 1, 1, 1, 1);
+        Console.WriteLine($"The sum is {sum3}");
+
+        Console.WriteLine("Give me a number");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Give me a number to multiply with the first one");
+        num2 = int.Parse(Console.ReadLine());
+
+        int product = Multiply(num1, num2);
+        Console.WriteLine($"The product is {product}");
+
+            int answer1 = Addition(500, 200);
+        Console.WriteLine(answer1);
+        int answer2 = Addition(1000, 200000);
+        
     }
    public static int Sum(int num1, int num2)
     {
@@ -38,4 +59,37 @@ internal class Program
     {
         return x * y;   
     }
+    public static int Sum1(params int[] list)
+    {
+        int sum1 = 0;
+        for(int i = 0; i< list.Length; i++)
+        {
+            sum1 = sum1 + list[i];
+        }
+        return sum1;
+    }
+    public static int Sum2(params int[] list)
+    {
+        int sum2 = 0;
+        for (int i = 0; i < list.Length; i++)
+        {
+            sum2 = sum2 + list[i];
+        }
+        return sum2;
+    }
+    public static int Sum3(params int[] list)
+    {
+        int sum3 = 0;
+        for (int i = 0; i < list.Length; i++)
+        {
+            sum3 = sum3 + list[i];
+        }
+        return sum3;
+    }
+
+    public static int Addition(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
 }
