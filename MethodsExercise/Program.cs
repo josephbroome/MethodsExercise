c1f396711998535629d26fcb66ad712bc9cdc86f
Joseph Broome <josephlloydbroome@gmail.com>
2022-06-02 09:59:42 -0700

    updates

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, All!");
+﻿// See https://aka.ms/new-console-template for more informatio
+Console.WriteLine("Hello, What is your name?");
+string userName = Console.ReadLine();
+
+Console.WriteLine("What is your dream car?");
+string carName  = Console.ReadLine();
+
+Console.WriteLine("What color is your dream car?");
+string carColor = Console.ReadLine();
+
+Console.WriteLine("Where is one place you always dreamed of visiting?");
+string dreamPlace = Console.ReadLine();
+
+
+Console.WriteLine($" There was once a person named {userName}. {userName} was very rich and owned a {carColor} {carName}. {userName} would always drive his {carName} around {dreamPlace}");    
